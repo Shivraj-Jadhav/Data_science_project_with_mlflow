240a6ca74e6832c13de94ddc446847d2bcbe6e3b
Shivraj-Jadhav <sj81898@gmail.com>
2024-08-19 07:03:15 +0530

    Model Validation and mlfow integration with dagshub

--- README.md
+++ README.md
@@ -12,3 +12,58 @@
 8. Pipeline
 9. main.py
 10. app.py
+
+
+
+# How to run?
+### STEPS:
+
+Clone the repository
+
+```bash
+https://github.com/Shivraj-Jadhav/Data_science_project_with_mlflow
+```
+### STEP 01- Create a conda environment after opening the repository
+
+```bash
+conda create -n mlproj python=3.8 -y
+```
+
+```bash
+conda activate mlproj
+```
+
+
+### STEP 02- install the requirements
+```bash
+pip install -r requirements.txt
+```
+
+
+```bash
+# Finally run the following command
+python app.py
+```
+
+Now,
+```bash
+open up you local host and port
+```
+
+
+
+## MLflow
+
+[Documentation](https://mlflow.org/docs/latest/index.html)
+
+
+##### cmd
+- mlflow ui
+
+### dagshub
+[dagshub](https://dagshub.com/)
+
+MLFLOW_TRACKING_URI=https://dagshub.com/Shivraj-Jadhav/Data_science_project_with_mlflow.mlflow
+
+
+
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from mlProject import logger, CustomException
 import sys
+import os
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_tansformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 logger.info("Welcome to the Project.\n\n")
@@ -50,3 +52,19 @@ try:
 
 except Exception as e:
     raise CustomException(e,sys)
+
+
+
+
+STAGE_NAME = "Model evaluation stage"
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/Shivraj-Jadhav/Data_science_project_with_mlflow.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="Shivraj-Jadhav"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="93dc3b35e8f9102932c713f62fc5f3d638a55ef5"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise CustomException(e,sys)
